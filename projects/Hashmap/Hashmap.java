dace8db204e6a5759429c6bc78a7645815a14d92
ポルト <118148112+hakkingu@users.noreply.github.com>
2023-03-08 17:01:19 +0000

    Create Hashmap.java

---
+++ projects/Hashmap/Hashmap.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Hashmap {
+  
+  public static void main (String [] args) {
+    
+    HashMap<String,String> hashmapper = new HashMap<String, String>();
+    hashmapper.put("otteru", "ilikevtubers");
+    hashmapper.put("caroru", "ilikemath");
+    hashmapper.put("potoru", "ilikeprogramming");
+    
+    System.out.println(hashmapper);
+    
+    }
+}
